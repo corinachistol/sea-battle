cff694aa2a95d4b90dadd973950ac869de2520ff
corina <chistolcorina19@gmail.com>
2024-04-29 10:36:22 +0300

    process.

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+
+
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello Exppress JS!")
+});
+
+// let coordinates = getCoords();
+// console.log(coordinates)
+app.listen(port ,()=>{
+    console.log(`Server is running on ${port}! `)
+})
+
+
+app.get('/shoot', (req,res) => {
+    console.log(req.query.shoot)
+    let {shoot} = req.query
+    console.log(shoot)
+
+    res.send({message:"coordinates received", shoot})
+
+})    
+
+
+
